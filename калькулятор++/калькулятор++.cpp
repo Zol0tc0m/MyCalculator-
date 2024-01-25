81914d019f08b3b42774942dec519fed83d865c7
Zol0tc0m <burmkirill@gmail.com>
2024-01-25 22:56:12 +0300

    Я создал проект и загрузил его

---
+++ калькулятор++/калькулятор++.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	setlocale(LC_ALL, "Russian");
+	int a, b;
+	cout << "Введите первое число: ";
+	cin >> a;
+	cout << "Введите второе число: ";
+	cin >> b;
+	int j = a * b;
+	int c = a / b;
+	int v = a + b;
+	int g = a - b;
+	cout << "сумма чисел = " << v << endl;
+	cout << "разность чисел = " << g << endl;
+	cout << "умножение чисел = " << j << endl;
+	cout << "деление чисел = " << c << endl;
+	return 0;
+}
